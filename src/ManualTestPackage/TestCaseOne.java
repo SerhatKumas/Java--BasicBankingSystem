cc360c6e5603a7599a85f174f4e760d903441eeb
Serhat Kumas <serhat.kumas@hotmail.com>
2022-11-30 01:34:47 +0300

    Manual test package is initialized

---
+++ src/ManualTestPackage/TestCaseOne.java
@@ -0,0 +1,20 @@
+package ManualTestPackage;
+
+import java.util.LinkedList;
+
+import BankPackage.Bank;
+import BankSystemPackage.BankingSystem;
+import ControllerPackage.BankAccountController;
+import ControllerPackage.BankController;
+import ControllerPackage.MemberController;
+import ControllerPackage.TransactionController;
+import MemberPackage.Member;
+
+public class TestCaseOne {
+
+	public static void main(String[] args) {
+		
+		
+	}
+	
+}
